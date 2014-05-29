fed33446b08c0e400a81735eb3d391beb634f256
Issam El Hachimi <issam.elhachimi@gmail.com>
2014-05-29 15:18:04 +0200

    Item

--- app/src/main/java/com/techies/towerdefensedelamorkitu/app/model/Item.java
+++ app/src/main/java/com/techies/towerdefensedelamorkitu/app/model/Item.java
@@ -3,15 +3,60 @@ package com.techies.towerdefensedelamorkitu.app.model;
 /**
  * Created by Issam on 29/05/2014.
  */
-public interface Item {
+public abstract class Item {
 
-    int x;
-    int y;
+    private int x;
+    private int y;
 
-    ItemType type;
+    private ItemType type;
 
     // temps 2
-    int destX;
-    int destY;
-    boolean isMoving;
+    private int destX;
+    private int destY;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public ItemType getType() {
+        return type;
+    }
+
+    public void setType(ItemType type) {
+        this.type = type;
+    }
+
+    public int getDestX() {
+        return destX;
+    }
+
+    public void setDestX(int destX) {
+        this.destX = destX;
+    }
+
+    public int getDestY() {
+        return destY;
+    }
+
+    public void setDestY(int destY) {
+        this.destY = destY;
+    }
+
+    public boolean isMoving() {
+        // TODO : UPDATE THIS.
+        return true;
+    }
+
 }
